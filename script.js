3a0822084d8f9db4f709711c182cbb987f3f0869
RobertooFelipe <feliperoberto092@gmail.com>
2022-01-06 20:57:47 -0300

    commit

--- script.js
+++ script.js
@@ -5,57 +5,57 @@ let valores = []
 
 
 
-function isNumero(n){
-    if(Number(n) >=1 && Number(n)<= 100){
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
-    }else{
+    } else {
         return false
     }
 }
 
-function inLista(n, l){
-    if(l.indexOf(Number(n)) != -1){
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
         return true
-    }else{
+    } else {
         return false
     }
 }
 
-function enviar(){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
+function enviar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         tab.appendChild(item)
         res.innerHTML = ''
-    }else{
+    } else {
         window.alert("Valor inválido ou ja adicionado")
     }
     num.value = ''
     num.focus()
 }
 
-function finalizar(){
+function finalizar() {
 
-    if (valores.length == 0){
+    if (valores.length == 0) {
         window.alert('Adicione valores antes de finalizar!')
-    }else{
+    } else {
         let tot = valores.length
         let maior = valores[0]
         let menor = valores[0]
         let soma = 0
         let media = 0
 
-            for(let pos in valores){
-                soma += valores[pos]
-                if(valores[pos] > maior){
-                    maior = valores[pos]
-                }
-                if(valores[pos] < menor){
-                    menor = valores[pos]
-                }
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
             }
-        media = soma/tot
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
